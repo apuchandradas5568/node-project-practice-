eefeaccaf3a5a5a5ffb974749e32f4c061a36e19
Apu Chandra Das <apuchandradas5568@gmail.com>
2024-02-11 23:39:24 +0530

    completed the jwt basics

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,6 +1,6 @@
 // check usrname, password in post login request
 
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const CustomAPIError = require("../errors/custom-error");
 // console.log(process.env.JWT_SECRET);
 //  if exists create new JWT
@@ -21,33 +21,33 @@ const login = async (req, res) => {
   }
   const id = new Date().getDate();
 
-  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: "30d" });
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
   res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
-    const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     throw new CustomAPIError("No token provided", 401);
-    }
+  }
 
-    const token = authHeader.split(' ')[1]
-    // console.log(token);
+  const token = authHeader.split(" ")[1];
+  // console.log(token);
 
-    try {
-const decoded = jwt.verify(token, process.env.JWT_SECRET)
-// console.log(decoded);
-const luckyNumber = Math.floor(Math.random() * 100);
-res.status(200).json({
-  msg: `Hello, ${decoded.username}`,
-  secret: `Here is your authorized data ${luckyNumber}`,
-});
-    } catch (error) {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // console.log(decoded);
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Here is your authorized data ${luckyNumber}`,
+    });
+  } catch (error) {
     throw new CustomAPIError("Not  authorized access to this route", 401);
-        
-    }
-
+  }
 };
 
 module.exports = {
