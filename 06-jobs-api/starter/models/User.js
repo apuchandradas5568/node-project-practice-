c1e17089a0beedc7fb2397c9c6e576c75d6a63b3
Apu Chandra Das <apuchandradas5568@gmail.com>
2024-02-12 16:21:54 +0530

    looking forward to login functionality

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,25 +1,47 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please Provide Name'],
-        minlength: 3,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        required: [true, 'Please Provide Mail'],
-        minlength: 3,
-        maxlength: 50,
-        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'please provide a valid email'],   unique: true
-    },
-    password: {
-        type: String,
-        required: [true, 'Please Provide password'],
-        minlength: 6,
-        maxlength: 12
-    }
-})
+  name: {
+    type: String,
+    required: [true, "Please Provide Name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please Provide Mail"],
+    minlength: 3,
+    maxlength: 50,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "please provide a valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please Provide password"],
+    minlength: 6,
+    // maxlength: 12
+  },
+});
 
-module.exports = mongoose.model('User', UserSchema)
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.getName = function () {
+  return this.name;
+};
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFELINE,
+  });
+};
+
+module.exports = mongoose.model("User", UserSchema);
